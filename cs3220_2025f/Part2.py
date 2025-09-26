3b409238a4d9d07a7f759a5feaedd2208542a2d6
GABRIEL NELLIS <5089401@nwpolytech.ca>
2025-09-26 00:56:02 -0600

    most of part 2 is working

--- cs3220_2025f/Part2.py
+++ cs3220_2025f/Part2.py
@@ -5,8 +5,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import networkx as nx
+#%matplotlib inline
 
-path="data/game-of-thrones-characters-groups.json"
+path = 'cs3220_2025f'
 
 json_files = [os.path.join(root, name) 
               for root, dirs, files in os.walk(path) 
@@ -14,14 +15,10 @@ json_files = [os.path.join(root, name)
               if name.endswith((".json"))] #If we needed to read several files extensions: if name.endswith((".ext1", ".ext2"))
 print('Number of JSON files ready to be loaded: ' + str(len(json_files)))
 
-#print('Path to the first file: '+json_files[0])
-with (open(path) as f):
-    json_data = path
-
-'''
+json_files
+print('Path to the first file: '+json_files[0])
 with (open(json_files[0]) as f):
     json_data = json.load(f)
-'''
 
 class Dynasty:
     def __init__(self,name):
@@ -49,7 +46,7 @@ class Dynasty:
 
     def __iter__(self): # to loop throw the list of characters via IN operator (for ex. for person in house: ....)
          for person in self.characters:
-             print(person + "\n")
+             yield person
 
     def __contains__(self, ch): # to check if the character belongs to the house (for ex., if person in house ...)
         for person in self.characters:
@@ -59,8 +56,9 @@ class Dynasty:
 
 
     def __str__(self): # to print like print(house) - > displat the house's name
-        print(self._name)
-    
+        printHouse = "This is a House of " + self._name + "!"
+        return printHouse
+
     def getStrength(self): # return N of family members in this house (int)
         return len(self.characters)
     
@@ -70,36 +68,37 @@ class GameOfThronesGraph:
         self.houses = {}
         #Load the house corpus
         for data_item in corpus:
-            house=Dynasty(data_item)
-            for characters in house:
+            house=Dynasty(data_item['name'])
+            for characters in data_item['characters']:
                 house.append(characters)
-            self.houses[data_item]=house
+            self.houses[data_item['name']]=house
             
 
     def __iter__(self): # for the case like the following: for house in GameOfThronesHouses:
          for house in self.houses.values():
-             print(house)
+             yield house
 
     def __contains__(self, h): #Check if h (house's name) is a key in dict houses - the house is in the graph
         if h in self.houses:
             return True
         return False
-'''
+
+#test 1
 for data in json_data['groups']:
   house=Dynasty(data['name'])
   for character in data['characters']:
-    house.append(character)
+    house.append(character) 
   print(house)
   print("Our members:")
   for person in house:
     print(person)
-  print(f"We have {house.getStrength()} family members!!!")
-
+  print(f"We have {house.getStrength()} family members!!!" + "\n") 
+#test 2
 corpus_data=json_data['groups']
 GameOfThronesHouses=GameOfThronesGraph(corpus_data)
 for house in GameOfThronesHouses:
    print(house)
-
+#test 3
 visualisationData={}
 legendData=[]
 for house in GameOfThronesHouses:
@@ -110,5 +109,60 @@ for house in GameOfThronesHouses:
 visualisationData
 legendData
 
-#%matplotlib inline
-'''
+#Configure your x and y values from the dictionary:
+x= list(visualisationData.keys())
+y=list(visualisationData.values())
+
+#Create the graph = create seaborn barplot
+ax=sns.barplot(x=x,y=y)
+
+#specfiy axis labels
+ax.legend(legendData)
+sns.move_legend(ax, "upper left", bbox_to_anchor=(1.05, 1))
+ax.set(xlabel='Houses',
+       ylabel='Strength (N family members)',
+       title='Strength of GameOfThronesHouses')
+
+plt.xticks(rotation=45)
+#display barplot
+plt.show()
+
+g=nx.Graph()
+
+N_houses=0
+colorKeys=[]
+for house in GameOfThronesHouses:
+    if house.name!="Include":
+        N_houses+=1
+        colorKeys.append(house.name)
+sns.color_palette("husl", N_houses) # N_houses colors
+
+nodeColors=dict(zip(colorKeys, [tuple(int(c*255) for c in cs) for cs in sns.color_palette("husl", N_houses)]))
+
+for house in GameOfThronesHouses:
+    if house.name!="Include":
+        # add the house's name as a node to the graph g (houses's strength values is used as a node's size)
+        g.add_node(house.name, size=house.getStrength())
+        
+for node, attributes in g.nodes(data=True): # run this code to check your code above
+    print(f"Node: {node}, Attributes: {attributes}")
+
+for house in GameOfThronesHouses:
+    if house.name!="Include":
+        # add each character as a node to the graph g 
+        for character in house: #goes through every character in the house to add them one by one
+            g.add_node(character)
+
+for node, attributes in g.nodes(data=True): # run this code to check your code above
+    print(f"Node: {node}, Attributes: {attributes}")
+
+myEdges=[]
+
+for house in GameOfThronesHouses:
+    if house.name!="Include":
+        for person in house:
+            myEdges.append(person, house)
+            
+
+print("Connections between a House and its family members:") # run this code to check your code above
+myEdges
